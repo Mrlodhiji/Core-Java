53f23723d4470893aad72f914a76f86273e0a814
Hariom Lodhi <155249241+Mrlodhiji@users.noreply.github.com>
2024-07-12 11:09:06 +0000

    DataTypes in java

---
+++ Basics/DataTypes.java
@@ -0,0 +1,22 @@
+package Basics;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        byte b = 10;// range -128 to 127
+        int x = 10;//range -2147483648 to 2147483647
+        char c= 'h'; // range 0 to 255
+        double d = 100.000;// range 15 digit decimal point
+        float f = 10.3f;
+        boolean isBoolean = true;//range-based on compiler
+        long l = 10000000000000000L;
+        short s = 12;//range -32768 to 32667
+        System.out.println(b);
+        System.out.println(x);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(f);
+        System.out.println(isBoolean);
+        System.out.println(l);
+        System.out.println(s);   
+     }
+}
