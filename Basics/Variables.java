8d6dd3f69a78343e8513b0ab522d9498a11fc8b9
Hariom Lodhi <155249241+Mrlodhiji@users.noreply.github.com>
2024-07-12 11:55:40 +0000

    variables in java

---
+++ Basics/Variables.java
@@ -0,0 +1,20 @@
+package Basics;
+
+/**
+ * var
+ */
+public class Variables {
+public static void main(String[] args) {
+    int a = 10;
+    int _a = 10;
+    // int ?a = 10;//not allowed
+    // int 0a = 10;//not allowed
+    int a$ = 10;//allowed
+    int $ =1;//allowed 
+    // int ?  = 1;//not allowed
+    System.out.println(a);
+    System.out.println(_a);
+    System.out.println(a$);
+    System.out.println($);
+}
+}
